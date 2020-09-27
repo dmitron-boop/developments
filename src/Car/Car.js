192f50e91031d32cfdb6c7917e9d5367d1ef6bee
Dmitry <zero.boop@gmail.com>
2020-09-27 19:34:21 +0300

    'css'

--- src/Car/Car.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default props => (
-  <div>
-    <h3>Сar name: {props.name}</h3>
-    <p>Price: <strong>{props.price}</strong> $</p>
-    { props.children }
-  </div>
-)
